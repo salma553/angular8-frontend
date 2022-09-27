669b72a38805426285c33ae23f8e7edd53863fad
salma553 <62328380+salma553@users.noreply.github.com>
2022-09-27 10:50:20 +0530

    Update env.js

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["apiUrl"] = "https://springboot-webapp.azurewebsites.net/api/employees";
+    window["env"]["apiUrl"] = "https://springboot-webapp.azurewebsites.net";
     window["env"]["debug"] = true;
   })(this);
